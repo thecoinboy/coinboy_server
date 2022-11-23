a55509d0ec86d17b2084686edfaa4e738a733f7e
coinboy <netsoft143@gmail.com>
2022-11-23 10:44:04 +0530

    cors update

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ import express from 'express'
 import bodyParser from 'body-parser';
 import { app } from './app.js'
 import router from './app.js'
+import cors from 'cors'
 
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use(express.json())
-app.use(express.json())
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.json());
+app.use(cors());
+
 app.use("/api/v1", router)
 
 const PORT = process.env.PORT || 5000;
